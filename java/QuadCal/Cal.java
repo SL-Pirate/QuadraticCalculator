563c753498be780444781dd1616e06d61b13f62f
SL-Pirate <80326180+SL-Pirate@users.noreply.github.com>
2022-05-26 20:50:46 +0530

    Update Cal.java
    Fixed some bugs related to implicit conversion of double to int of value zero `0.0 -> 0
    Various security and performance optimizations

--- java/QuadCal/Cal.java
+++ java/QuadCal/Cal.java
@@ -1,196 +1,170 @@
 package QuadCal;
 import java.text.DecimalFormat;
 
-public class Cal{
-
-	private double a;
-	private double b;
-	private double c;
-	private double root1;
-	private double root2;
+public class Cal {
+	private final double a;
+	private final double b;
+	private final double c;
+	private double[] roots;
 	private double discriminant;
+	private String equation;
+	private String range;
+	private String MinOrMax;
+	private String nature;
+	private int code = -1;
+	/*  code = -1 or 3 when there's an error
+		code = 0 when there are no real roots
+		code = 1 when there's a single coincident root
+		code = 2 when there are two distinct real roots
+	 */
 
-	public Cal(double a, double b, double c) throws notQuadraticException{
-		setA(a);
-		setB(b);
-		setC(c);
-		set_discriminant();
+	public Cal(double a, double b, double c) throws notQuadraticException {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		discriminant();
+		eqn();
 		roots();
+		nature();
+		range();
+		MinOrMax();
 	}
 
-	public String df(double x) {
+	private String df(double x) {
 		DecimalFormat decimal = new DecimalFormat("#.##");
 		return decimal.format(x);
 	}
 
-	public double discriminant() {
-		return b*b-4*a*c;
+	private void discriminant() {
+		discriminant = b * b - 4 * a * c;
 	}
 
-	public void set_discriminant() {
-		this.discriminant = discriminant();
+	private void eqn() throws notQuadraticException {
+		String aStr;
+		String bStr;
+		String cStr;
+
+		String BtoS = df(b);
+		String CtoS = df(c);
+
+		if (a == 1.0) {
+			aStr = "";
+		} else if (a == -1.0) {
+			aStr = "-";
+		} else if (a == 0.0) {
+			throw new notQuadraticException();
+		} else {
+			aStr = df(a);
+		}
+
+		if (b == 1.0) {
+			bStr = "+";
+		} else if (b == -1.0) {
+			bStr = "-";
+		} else if (b == 0.0) {
+			bStr = null;
+		} else if (b > 0.0) {
+			bStr = "+" + BtoS;
+		} else {
+			bStr = BtoS;
+		}
+
+		if (c == 0.0) {
+			cStr = "";
+		} else if (c > 0.0) {
+			cStr = "+" + CtoS;
+		} else {
+			cStr = CtoS;
+		}
+
+		if (bStr == null) {
+			equation = "y=" + aStr + cStr;
+		} else {
+			equation = "y=" + aStr + "x\u00b2" + bStr + "x" + cStr;
+		}
 	}
 
-	public String eqn() {
-		String a_tmp = df(a);
-		String b_tmp = df(b);
-		String c_tmp = df(c);
-
-		switch (a_tmp) {
-			case "-1": a_tmp = "-";
-			break;
-			case "1": a_tmp = "";
-		}
-		switch (b_tmp) {
-			case "-1": b_tmp = "-";
-			break;
-			case "0": b_tmp = "NaN";
-			break;
-			case "1": b_tmp = "+";
-		}
-		if (b > 0 && b!=1) {
-			b_tmp = "+"+b_tmp;
-		}
-		if (c==0) {
-			c_tmp = "NaN";
-		}
-		else if(c>0) {
-			c_tmp = "+"+c_tmp;
-		}
-
-
-		if(b_tmp.equals("NaN") && c_tmp.equals("NaN")) {
-			return "y="+a_tmp+"x\u00b2";
-		}
-		else if (b_tmp.equals("NaN")) {
-			return "y="+a_tmp+"x\u00b2"+c_tmp;
-		}
-		else if(c_tmp.equals("NaN")){
-			return "y="+a_tmp+"x\u00b2"+b_tmp+"x";
-		}
-		else {
-		return "y="+a_tmp+"x\u00b2"+b_tmp+"x"+c_tmp;
-		}
-	}
-
-	public String sign() {
-		String out;
-		String lroot1 = df(root1);
-		String lroot2 = df(root2);
+	private void range() {
+		String lroot1 = df(roots[0]);
+		String lroot2 = df(roots[1]);
 
 		if (discriminant > 0 && a > 0) {
-			out = "positive in the range of "+lroot1+" > x > "+lroot2;
-		}
-		else if(discriminant > 0 && a<0) {
-			out = "positive in the range of "+lroot1+" < x U x < "+lroot2;
-		}
-		else if(discriminant <0 && a>0) {
-			out = "positive for all x real";
-		}
-		else if(discriminant<0 && a<0) {
-			out = "negative for all x real";
-		}
-		else if(discriminant==0 && a>0){
-			out = "positive for all x real \n\tand equal to zero at " + lroot1;
-		}
-		else if(discriminant==0 && a<0){
-			out = "negative for all x real \n\tand equal to zero at " + lroot1;
-		}
-		else {
-			out = null;
-		}
-
-		return out;
-	}
-
-	public String minORmax() {
-		if (a>0) {
-			return "minimum";
-		}
-		else if (a<0){
-			return "maximum";
-		}
-		else {
-			return null;
+			range = "positive in the range of " + lroot1 + " > x > " + lroot2;
+		} else if (discriminant > 0 && a < 0) {
+			range = "positive in the range of " + lroot1 + " < x U x < " + lroot2;
+		} else if (discriminant < 0 && a > 0) {
+			range = "positive for all x real";
+		} else if (discriminant < 0 && a < 0) {
+			range = "negative for all x real";
+		} else if (discriminant == 0 && a > 0) {
+			range = "positive for all x real \n\tand equal to zero at " + lroot1;
+		} else if (discriminant == 0 && a < 0) {
+			range = "negative for all x real \n\tand equal to zero at " + lroot1;
+		} else {
+			range = null;
 		}
 	}
 
-	public String[] roots() {
+	private void MinOrMax() {
+		if (a > 0) {
+			MinOrMax = "minimum";
+		} else if (a < 0) {
+			MinOrMax = "maximum";
+		} else {
+			MinOrMax = null;
+		}
+	}
+
+	private void roots() {
+		roots = new double[2];
 		double sqrt_disc = Math.sqrt(discriminant);
-
-		root1 = (-b-sqrt_disc)/(2*a);
-		root2 = (-b+sqrt_disc)/(2*a);
-
 		double tmp;
-		if (root1 < root2){
-			tmp = root1;
-			root1 = root2;
-			root2 = tmp;
+
+		roots[0] = (-b - sqrt_disc) / (2 * a);
+		roots[1] = (-b + sqrt_disc) / (2 * a);
+
+		if (roots[0] < roots[1]) {
+			tmp = roots[0];
+			roots[0] = roots[1];
+			roots[1] = tmp;
 		}
-
-		String Sroot1 = Double.toString(root1);
-		String Sroot2 = Double.toString(root2);
-
-		if (Sroot1.equals(Sroot2)) {
-			Sroot2="NaN";
-		}
-
-		return new String[] {Sroot1, Sroot2};
 	}
 
-	public String[] nature() {
-		String nat;
-		String code;
-
+	private void nature() {
 		if (discriminant < 0) {
-			nat = "no real roots";
-			code = "0";
+			nature = "no real roots";
+			code = 0;
+		} else if (discriminant == 0) {
+			nature = "one coincident root";
+			code = 1;
+		} else if (discriminant > 0) {
+			nature = "two distinct real roots";
+			code = 2;
+		} else {
+			code = 3;
 		}
-		else if (discriminant == 0) {
-			nat = "one coincident root";
-			code = "1";
-		}
-		else if (discriminant > 0) {
-			nat = "two distinct real roots";
-			code = "2";
-		}
-		else {
-			nat = null;
-			code = "3";
-		}
-
-		return new String[] {nat, code};
 	}
 
-	public String[] run() {
-		String[] out = new String[8];
-		out[0] = eqn();
-		String[] nature = nature();
-		String[] root = roots();
-		out[1] = Double.toString(discriminant);
-		out[2] = nature[0];
-		out[3] = root[0];
-		out[4] = root[1];
-		out[5] = nature[1];
-		out[6] = sign();
-		out[7] = minORmax();
-		return out;
+	//getters
+	public String getEquation(){
+		return equation;
 	}
-
-	public String toString() {
-		return eqn()+"\n"+discriminant+"\n"+nature()[0]+"\n"+root1+"\n"+root2;
+	public double getRoot(int i){
+		return roots[i];
 	}
-	
-	public void setA(double a) throws notQuadraticException{
-		if (a == 0) {
-			throw (new notQuadraticException());
-		}
-		this.a = a;
+	public String getRange(){
+		return range;
 	}
-	public void setB(double b) {
-		this.b = b;
+	public double getDiscriminant(){
+		return discriminant;
 	}
-	public void setC(double c) {
-		this.c = c;
+	public String getMinOrMax(){
+		return MinOrMax;
+	}
+	public String getNature(){
+		return nature;
+	}
+	public int getCode(){
+		return code;
 	}
 }
